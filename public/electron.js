9e26daa8ab56d9f834be13cf8840897a847af22f
Howard <howardshen2306@gmail.com>
2022-08-31 14:10:59 +0800

    feat: update application pixl

--- public/electron.js
+++ public/electron.js
@@ -6,8 +6,8 @@ const isDev = require('electron-is-dev');
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1100,
+    height: 650,
     webPreferences: {
       nodeIntegration: true,
     },
