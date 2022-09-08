c264e353b74cf46f2dc5bed4e1592b56c9f94e98
Howard <howardshen2306@gmail.com>
2022-09-08 13:49:34 +0800

    feat: refactor code layout

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import DirectMessage from './components/Direct_message';
-import Servers from './components/Servers';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <>
-    <DirectMessage />
-    <Servers />
-  </>
-
+  <App />
   // </React.StrictMode>
 );
