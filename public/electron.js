7da7c3a0a556187c1dac70dc5c214b21c9ce6562
Howard <howardshen2306@gmail.com>
2022-09-08 13:48:53 +0800

    feat: update application layout

--- public/electron.js
+++ public/electron.js
@@ -13,16 +13,20 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
-  win.setMaximumSize(1100, 650);
+  win.setMaximumSize(1600, 900);
   win.maximizable = false;
 
   // and load the index.html of the app.
   // win.loadFile("index.html");
-  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
   // Open the DevTools.
-  if (isDev) {
-    win.webContents.openDevTools({ mode: 'detach' });
-  }
+  // if (isDev) {
+  //   win.webContents.openDevTools({ mode: 'detach' });
+  // }
 }
 
 // This method will be called when Electron has finished
