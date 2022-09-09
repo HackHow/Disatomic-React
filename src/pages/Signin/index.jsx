72adc2be3371ef7f39a8192222ce578441c5fe53
Howard <howardshen2306@gmail.com>
2022-09-10 00:53:18 +0800

    feat: add signin page global component

---
+++ src/pages/Signin/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from './component/Signin';
+
+function SignInInterface() {
+  return (
+    <>
+      <SignIn />
+    </>
+  );
+}
+
+export default SignInInterface;
