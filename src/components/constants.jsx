0e32464c3d34673a99ffd1ab53f60d478d6d2697
Howard <howardshen2306@gmail.com>
2022-09-10 00:51:43 +0800

    feat: add global constants

---
+++ src/components/constants.jsx
@@ -0,0 +1,7 @@
+const DNS = 'http://localhost:3001';
+const REGISTER_URL = `${DNS}/api/1.0/user/signup`;
+const LOGIN_URL = `${DNS}/api/1.0/user/signin`;
+
+const constants = { REGISTER_URL, LOGIN_URL };
+
+export default constants;
