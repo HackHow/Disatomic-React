7e7f52636ac459aaf316f6fb3227acda44c5af76
Howard <howardshen2306@gmail.com>
2022-09-10 00:47:00 +0800

    feat: add React Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,22 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SideBar from './components/SideBar';
 import ChatInterface from './pages/Chat_interface';
-import Signin from './pages/Signin';
-import styled from 'styled-components';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+// import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const AppContainer = styled.div`
-  #container {
-    display: flex;
-  }
-`;
 
 function App() {
   return (
-    <AppContainer>
-      <div id='container'>
-        <SideBar />
-        <BrowserRouter>
-          <Routes>
-            <Route path='/signin' element={<Signin />} />
-            <Route path='/channels' element={<ChatInterface />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </AppContainer>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<SignIn />} />
+        <Route path='/register' element={<SignUp />} />
+        <Route path='/server' element={<SideBar />}>
+          <Route path='channels' element={<ChatInterface />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
