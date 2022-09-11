2550812b6d48c58839b1689846ac82d204fe6045
Howard <howardshen2306@gmail.com>
2022-09-11 20:59:59 +0800

    feat: add home page component and direct login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SideBar from './components/SideBar';
 import ChatInterface from './pages/ChatInterface';
 import LoginInterface from './pages/LoginInterface';
 import RegisterInterface from './pages/RegisterInterface';
+import HomeInterface from './pages/HomeInterface';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [userId, setUserId] = useState('');
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<LoginInterface />} />
+        <Route path='/' element={<Navigate to='/login' />} />
+        <Route
+          path='/login'
+          element={<LoginInterface setUserId={setUserId} />}
+        />
         <Route path='/register' element={<RegisterInterface />} />
         <Route path='/server' element={<SideBar />}>
+          <Route path='home' element={<HomeInterface userId={userId} />} />
           <Route path='channels' element={<ChatInterface />} />
         </Route>
       </Routes>
