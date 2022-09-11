7ba9443fb2d8f4a63cca2ebbce2427b923502e5d
Howard <howardshen2306@gmail.com>
2022-09-11 21:02:29 +0800

    feat: add invite friend api url

--- src/components/constants.jsx
+++ src/components/constants.jsx
@@ -1,7 +1,8 @@
 const DNS = 'http://localhost:3001';
 const REGISTER_URL = `${DNS}/api/1.0/user/signup`;
 const LOGIN_URL = `${DNS}/api/1.0/user/signin`;
+const ADD_FRIEND_URL = `${DNS}/api/1.0/user/friend`;
 
-const constants = { REGISTER_URL, LOGIN_URL };
+const constants = { REGISTER_URL, LOGIN_URL, ADD_FRIEND_URL };
 
 export default constants;
