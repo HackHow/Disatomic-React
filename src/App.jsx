0a69714d8d80429191434e96826cab8019e30c61
Howard <howardshen2306@gmail.com>
2022-09-15 20:54:53 +0800

    feat: delete useState about userId and setUserId

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,14 @@ import HomeInterface from './pages/HomeInterface';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [userId, setUserId] = useState('');
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Navigate to='/login' />} />
-        <Route
-          path='/login'
-          element={<LoginInterface setUserId={setUserId} />}
-        />
+        <Route path='/login' element={<LoginInterface />} />
         <Route path='/register' element={<RegisterInterface />} />
-        <Route path='/server' element={<SideBar userId={userId} />}>
-          <Route path='home' element={<HomeInterface userId={userId} />} />
+        <Route path='/server' element={<SideBar />}>
+          <Route path='home' element={<HomeInterface />} />
           <Route path='channels' element={<ChatInterface />} />
         </Route>
       </Routes>
