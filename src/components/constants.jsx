b361bfc2b3c1ffd126c4381c4b7c80bf2d902340
Howard <howardshen2306@gmail.com>
2022-09-15 20:56:41 +0800

    feat: add userInfo api url

--- src/components/constants.jsx
+++ src/components/constants.jsx
@@ -3,7 +3,14 @@ const REGISTER_URL = `${DNS}/api/1.0/user/signup`;
 const LOGIN_URL = `${DNS}/api/1.0/user/signin`;
 const ADD_FRIEND_URL = `${DNS}/api/1.0/user/friend`;
 const CREATE_SERVER = `${DNS}/api/1.0/server`;
+const USER_INFO = `${DNS}/api/1.0/user`;
 
-const constants = { REGISTER_URL, LOGIN_URL, ADD_FRIEND_URL, CREATE_SERVER };
+const constants = {
+  REGISTER_URL,
+  LOGIN_URL,
+  ADD_FRIEND_URL,
+  CREATE_SERVER,
+  USER_INFO,
+};
 
 export default constants;
