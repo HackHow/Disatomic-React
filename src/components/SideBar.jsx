e7cd9aaa54e9fcda96b0583b423bf9234edbd801
Howard <howardshen2306@gmail.com>
2022-09-15 20:56:20 +0800

    test: test user own server api

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from '../assets/logo.png';
@@ -31,31 +31,47 @@ const SideBarContainer = styled.div`
     display: flex;
     flex-direction: column;
     span {
-      font-size: larger;
-      font-weight: bold;
+      /* font-size: larger; */
+      /* font-weight: bold; */
     }
   }
 `;
 
 function SideBar({ userId }) {
-  const [demoServer, setDemoServer] = useState('');
+  const [servers, setServers] = useState([]);
 
-  console.log('userId', userId);
-
-  const createServer = async () => {
-    const serverName = 'AppWorkSchool';
-    // const userId = '631f1fe4a3e26e83aaa525e2';
-    const data = { userId, serverName };
-    const url = Constants.CREATE_SERVER;
-    console.log('click pass');
-    const result = await axios.post(url, data);
-
-    console.log('result', result);
-    if (result.data === 'Create server success') {
-      const testServer = 'Test Server';
-      setDemoServer(testServer);
+  useEffect(() => {
+    const url = Constants.USER_INFO;
+    const token = localStorage.getItem('Authorization');
+    try {
+      const getUserInfo = async () => {
+        const { data } = await axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setServers(data.userOwnServers);
+      };
+      getUserInfo();
+    } catch (error) {
+      console.log(error.message);
     }
-  };
+  }, []);
+
+  // const createServer = async () => {
+  //   const serverName = 'AppWorkSchool';
+  //   // const userId = '631f1fe4a3e26e83aaa525e2';
+  //   const data = { userId, serverName };
+  //   const url = Constants.CREATE_SERVER;
+  //   console.log('click pass');
+  //   const result = await axios.post(url, data);
+
+  //   console.log('result', result);
+  //   if (result.data === 'Create server success') {
+  //     const testServer = 'Test Server';
+  //     setDemoServer(testServer);
+  //   }
+  // };
 
   return (
     <SideBarContainer>
@@ -66,16 +82,19 @@ function SideBar({ userId }) {
           </button>
         </div>
         <div className='add-server'>
+          <ol>
+            <li>
+              {servers
+                ? servers.map((item) => (
+                    <button type='submit' className='server'>
+                      <span>{item}</span>
+                    </button>
+                  ))
+                : null}
+            </li>
+          </ol>
           <button type='submit' className='server'>
-            <span>AppWork School</span>
-          </button>
-          {demoServer && (
-            <button type='submit' className='server'>
-              <span>{demoServer}</span>
-            </button>
-          )}
-          <button type='submit' className='server'>
-            <span onClick={createServer}>+</span>
+            <span>+</span>
           </button>
         </div>
       </div>
