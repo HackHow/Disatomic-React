187c14479bf639660337d2dfadbb6eb130aa86db
Howard <howardshen2306@gmail.com>
2022-09-16 18:03:14 +0800

    feat: modify react component

--- src/pages/HomeInterface/index.jsx
+++ src/pages/HomeInterface/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddFriend from './component/AddFriend';
 import DirectMessages from './component/DirectMessages';
 import FriendStatus from './component/FriendStatus';
 
 function HomeInterface() {
   return (
     <>
+      <AddFriend />
       <DirectMessages />
       <FriendStatus />
     </>
