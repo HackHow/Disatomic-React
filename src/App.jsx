c320e61aa6576f05f49bf2a5120aa7211e2e8af3
Howard <howardshen2306@gmail.com>
2022-09-16 12:03:59 +0800

    feat: update no use package and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SideBar from './components/SideBar';
 import ChatInterface from './pages/ChatInterface';
