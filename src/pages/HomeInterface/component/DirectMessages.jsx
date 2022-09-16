2279e36391413eb6a0d3b54583170483ce6d3b56
Howard <howardshen2306@gmail.com>
2022-09-16 18:04:42 +0800

    feat: move code to AddFriend.jsx

--- src/pages/HomeInterface/component/DirectMessages.jsx
+++ src/pages/HomeInterface/component/DirectMessages.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-// import * as BsIcons from 'react-icons/bs';
 import CreateMessage from './CreateMessage';
-import AddFriend from './AddFriend';
 
 const Home = styled.div`
   margin-left: 15px;
@@ -15,38 +13,13 @@ const PrivateMessage = styled.div`
   /* width: 600px; */
 `;
 
-const FriendsIcon = styled.div`
-  border: 1px solid #010000;
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-`;
-
-const FriendsList = styled.div`
-  display: flex;
-  &:hover {
-    color: gray;
-  }
-`;
-
-function DirectMessages({ userId }) {
+function DirectMessages() {
   return (
     <Home>
-      <div>
-        <AddFriend userId={userId} />
-      </div>
-
       <PrivateMessage>
         <div>私人訊息</div>
         <CreateMessage />
       </PrivateMessage>
-
-      {/* <FriendsList>
-        <FriendsIcon>
-          <div></div>
-        </FriendsIcon>
-        <div>Morton</div>
-      </FriendsList> */}
     </Home>
   );
 }
