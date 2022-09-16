3b36d5fdf258c8904c76b050f0f6d6890f96c888
Howard <howardshen2306@gmail.com>
2022-09-16 12:04:27 +0800

    feat: move server component to CreateServer.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from '../assets/logo.png';
-import axios from 'axios';
-import Constants from './Constants';
+import CreateServer from './CreateServer';
 
 const SideBarContainer = styled.div`
   padding: 10px 10px;
@@ -30,49 +29,10 @@ const SideBarContainer = styled.div`
   .add-server {
     display: flex;
     flex-direction: column;
-    span {
-      /* font-size: larger; */
-      /* font-weight: bold; */
-    }
   }
 `;
 
-function SideBar({ userId }) {
-  const [servers, setServers] = useState([]);
-
-  useEffect(() => {
-    const url = Constants.USER_INFO;
-    const token = localStorage.getItem('Authorization');
-    try {
-      const getUserInfo = async () => {
-        const { data } = await axios.get(url, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        setServers(data.userOwnServers);
-      };
-      getUserInfo();
-    } catch (error) {
-      console.log(error.message);
-    }
-  }, []);
-
-  // const createServer = async () => {
-  //   const serverName = 'AppWorkSchool';
-  //   // const userId = '631f1fe4a3e26e83aaa525e2';
-  //   const data = { userId, serverName };
-  //   const url = Constants.CREATE_SERVER;
-  //   console.log('click pass');
-  //   const result = await axios.post(url, data);
-
-  //   console.log('result', result);
-  //   if (result.data === 'Create server success') {
-  //     const testServer = 'Test Server';
-  //     setDemoServer(testServer);
-  //   }
-  // };
-
+function SideBar() {
   return (
     <SideBarContainer>
       <div>
@@ -81,22 +41,7 @@ function SideBar({ userId }) {
             <img src={Logo} alt='Logo' />
           </button>
         </div>
-        <div className='add-server'>
-          <ol>
-            <li>
-              {servers
-                ? servers.map((item) => (
-                    <button type='submit' className='server'>
-                      <span>{item}</span>
-                    </button>
-                  ))
-                : null}
-            </li>
-          </ol>
-          <button type='submit' className='server'>
-            <span>+</span>
-          </button>
-        </div>
+        <CreateServer></CreateServer>
       </div>
       <Outlet />
     </SideBarContainer>
