a74f0390ac2b5167a2ac7ff6876febafcb3fa29b
Howard <howardshen2306@gmail.com>
2022-09-19 08:52:36 +0800

    feat: modify css attribute and move code to parents

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,63 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Logo from '../assets/logo.png';
 import styled from 'styled-components';
-import io from 'socket.io-client';
 import Constants from './Constants';
 import axios from 'axios';
 
-const SideBarContainer = styled.div`
-  padding: 10px 10px;
-  display: flex;
-  .server {
-    border: none;
-    height: 100px;
-    width: 100px;
-    border-radius: 80%;
-    background-color: #808080bb;
-    color: white;
-    cursor: pointer;
-    &:hover {
-      background-color: #7e89c8;
-      border-radius: 30%;
-    }
-  }
-  .direct-messages {
-    img {
-      height: 100px;
-      width: 100px;
-    }
-  }
-  .add-server {
-    display: flex;
-    flex-direction: column;
+const LogoContainer = styled.img`
+  height: 100px;
+  width: 100px;
+  border: none;
+  height: 100px;
+  width: 100px;
+  border-radius: 80%;
+  background-color: #808080bb;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #7e89c8;
+    border-radius: 30%;
   }
 `;
-
-// const token = localStorage.getItem('Authorization');
-// const socket = io.connect('http://localhost:3001/', {
-//   auth: {
-//     token: `Bearer ${token}`,
-//   },
-//   // withCredentials: true,
-// });
+const ServersContainer = styled.div`
+  text-align: center;
+  padding: 40px 0;
+  height: 100px;
+  width: 100px;
+  border: none;
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  background-color: #808080bb;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #7e89c8;
+    border-radius: 30%;
+  }
+`;
 
 function SideBar() {
   const [show, setShow] = useState(false);
   const [serversArray, setServersArray] = useState([]);
   const [serverName, setServerName] = useState('');
-  const [userId, setUserId] = useState('');
   const token = localStorage.getItem('Authorization');
-
-  // const socket = io.connect('http://localhost:3001/', {
-  //   auth: {
-  //     token: `Bearer ${token}`,
-  //   },
-  //   // withCredentials: true,
-  // });
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -76,41 +63,12 @@ function SideBar() {
           },
         });
         setServersArray(data.userOwnServers);
-        setUserId(data.userId);
       };
       getUserInfo();
     } catch (error) {
       console.log(error.message);
     }
   }, []);
-
-  useEffect(() => {
-    const socket = io.connect('http://localhost:3001/', {
-      auth: {
-        token: `Bearer ${token}`,
-      },
-      withCredentials: true,
-    });
-
-    socket.on('connect', () => {
-      console.log('socket connected:', socket.id, new Date().toISOString());
-    });
-
-    socket.on('disconnect', () => {
-      console.log('socket disconnect');
-    });
-
-    socket.on('friendState', (data) => console.log(data));
-
-    return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-    };
-  }, []);
-
-  // useEffect(() => {
-  //   socket.on('Hello', (data) => console.log(data));
-  // }, []);
 
   const createServerClick = async () => {
     const url = Constants.CREATE_SERVER;
@@ -145,62 +103,54 @@ function SideBar() {
 
   return (
     <>
-      <SideBarContainer>
+      {/* <SideBarContainer> */}
+      <div>
+        <LogoContainer src={Logo} alt='Logo'></LogoContainer>
+
         <div>
-          <div className='direct-messages'>
-            <button className='server' type='submit'>
-              <img src={Logo} alt='Logo' />
-            </button>
-          </div>
-          <div className='add-server'>
-            <ol>
-              {serversArray &&
-                serversArray.map((item) => (
-                  <button type='submit' className='server'>
-                    <li>{item}</li>
-                  </button>
-                ))}
-            </ol>
-          </div>
-
-          <Button variant='primary' onClick={handleShow}>
-            +
-          </Button>
-
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>新增伺服器</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form>
-                <Form.Group
-                  className='mb-3'
-                  controlId='exampleForm.ControlInput1'
-                >
-                  <Form.Label>輸入伺服器名稱</Form.Label>
-                  <Form.Control
-                    type='text'
-                    placeholder='AppWorkSchool'
-                    // value='AppWorkSchool'
-                    onChange={changeHandler}
-                    onKeyDown={handleKeypress}
-                    autoFocus
-                  />
-                </Form.Group>
-              </Form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant='primary' onClick={createServerClick}>
-                建立
-              </Button>
-              <Button variant='secondary' onClick={handleClose}>
-                取消
-              </Button>
-            </Modal.Footer>
-          </Modal>
+          {serversArray &&
+            serversArray.map((item) => (
+              <ServersContainer>{item}</ServersContainer>
+            ))}
         </div>
-        <Outlet />
-      </SideBarContainer>
+
+        <Button variant='primary' onClick={handleShow}>
+          +
+        </Button>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>新增伺服器</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group
+                className='mb-3'
+                controlId='exampleForm.ControlInput1'
+              >
+                <Form.Label>輸入伺服器名稱</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='AppWorkSchool'
+                  // value='AppWorkSchool'
+                  onChange={changeHandler}
+                  onKeyDown={handleKeypress}
+                  autoFocus
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant='primary' onClick={createServerClick}>
+              建立
+            </Button>
+            <Button variant='secondary' onClick={handleClose}>
+              取消
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+      {/* </SideBarContainer> */}
     </>
   );
 }
