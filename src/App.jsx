622dcd3138b21dacece8e5ae8ff4dfe1c7172cc8
Howard <howardshen2306@gmail.com>
2022-09-19 08:51:20 +0800

    feat: modify component location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import SideBar from './components/SideBar';
 import ChatInterface from './pages/ChatInterface';
 import LoginInterface from './pages/LoginInterface';
 import RegisterInterface from './pages/RegisterInterface';
@@ -14,10 +13,8 @@ function App() {
         <Route path='/' element={<Navigate to='/login' />} />
         <Route path='/login' element={<LoginInterface />} />
         <Route path='/register' element={<RegisterInterface />} />
-        <Route path='/server' element={<SideBar />}>
-          <Route path='home' element={<HomeInterface />} />
-          <Route path='channels' element={<ChatInterface />} />
-        </Route>
+        <Route path='/channels/@me' element={<HomeInterface />} />
+        <Route path='/channels/:serverId' element={<ChatInterface />} />
       </Routes>
     </BrowserRouter>
   );
