dd89ab713e5c17b372eb34a0bb9b3c632aba0984
Howard <howardshen2306@gmail.com>
2022-09-19 09:33:56 +0800

    feat: delete no use package

--- src/pages/HomeInterface/component/FriendStatus.jsx
+++ src/pages/HomeInterface/component/FriendStatus.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Constants from '../../../components/Constants';
 
