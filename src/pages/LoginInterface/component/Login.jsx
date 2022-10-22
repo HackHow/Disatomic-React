269b13529fbdf7c98a076728aaf5f6b1f1845996
Howard <howardshen2306@gmail.com>
2022-10-22 15:43:14 +0800

    feat: update layout

--- src/pages/LoginInterface/component/Login.jsx
+++ src/pages/LoginInterface/component/Login.jsx
@@ -5,39 +5,82 @@ import axios from 'axios';
 import Constants from '../../../components/Constants';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import universe from '../../../assets/universe.jpg';
 
 const SignInContainer = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  background-image: url(${universe});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 const TitleContainer = styled.h3`
   text-align: center;
+  font-size: 40px;
+  margin-bottom: 40px;
+  color: white;
 `;
 
 const InputDataContainer = styled.label`
   display: block;
+  font-size: 22px;
+  margin: 10px 0;
+  font-style: italic;
+  color: #eaeff2;
 `;
 
-const RegisterButton = styled.button`
+const LoginButton = styled.button`
   display: block;
-  margin-top: 20px;
-  border-radius: 16px 0;
+  margin-top: 40px;
+  border-radius: 0 16px;
+  border: 3px outset;
+  font-size: 23px;
   cursor: pointer;
+  &:hover {
+    background-color: var(--link);
+  }
 `;
 
 const ButtonAndLink = styled.div`
   display: flex;
   flex-direction: column;
+
+  > span {
+    font-size: 18px;
+    margin-top: 30px;
+    color: white;
+  }
 `;
 
 const Input = styled.input`
-  /* border: solid; */
-  padding: 0 16px;
-  border-radius: 16px 0;
+  border: 2px groove;
+  border-radius: 0 16px;
+  border-width: 5px;
+  padding: 3px 14px;
+  margin-bottom: 10px;
+  font-size: 23px;
+`;
+
+const FormLogin = styled.div`
+  background-color: var(--primary);
+  border-radius: 16px;
+  padding: 60px 80px;
+  opacity: 0.75;
+`;
+
+const StyledLink = styled(Link)`
+  color: #04f1bd;
+  padding-left: 5px;
+  font-size: 18px;
+
+  &:hover {
+    color: #f62fdd;
+  }
 `;
 
 function Login() {
@@ -67,19 +110,25 @@ function Login() {
       localStorage.setItem('Authorization', data.accessToken);
       navigate('/channels/@me');
     } catch (error) {
-      // console.log(error.response.data);
-      // alert(error.response.data);
-      MySwal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        html: `<b>${error.response.data}</b>`,
-      });
+      // console.log(error);
+      if (error.response.data !== undefined) {
+        MySwal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          html: `<b>${error.response.data}</b>`,
+        });
+      } else {
+        MySwal.fire({
+          icon: 'error',
+          title: 'Please contact us if you are unable to login',
+        });
+      }
     }
   };
 
   return (
     <SignInContainer>
-      <div>
+      <FormLogin>
         <TitleContainer>歡迎回來</TitleContainer>
         <form onSubmit={loginButton}>
           <InputDataContainer>電子郵件</InputDataContainer>
@@ -100,13 +149,14 @@ function Login() {
             required
           ></Input>
           <ButtonAndLink>
-            <RegisterButton type='submit'>登入</RegisterButton>
+            <LoginButton type='submit'>登入</LoginButton>
             <span>
-              需要一個帳號？<Link to='/register'>註冊</Link>
+              需要一個帳號？
+              <StyledLink to='/register'>註冊</StyledLink>
             </span>
           </ButtonAndLink>
         </form>
-      </div>
+      </FormLogin>
     </SignInContainer>
   );
 }
--- src/pages/RegisterInterface/component/Register.jsx
+++ src/pages/RegisterInterface/component/Register.jsx
@@ -5,38 +5,86 @@ import axios from 'axios';
 import Constants from '../../../components/Constants';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import universe from '../../../assets/universe.jpg';
 
 const SignUpContainer = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  background-image: url(${universe});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 const TitleContainer = styled.h3`
   text-align: center;
+  font-size: 40px;
+  margin-bottom: 40px;
+  color: white;
 `;
 
 const InputDataContainer = styled.label`
   display: block;
+  font-size: 22px;
+  margin: 10px 0;
+  font-style: italic;
+  color: #eaeff2;
 `;
 
 const RegisterButton = styled.button`
   display: block;
-  margin-top: 20px;
-  border-radius: 16px 0;
+  margin-top: 40px;
+  border-radius: 0 16px;
+  border: 3px outset;
+  font-size: 23px;
+  cursor: pointer;
+  &:hover {
+    background-color: var(--link);
+  }
 `;
 
 const ButtonAndLink = styled.div`
   display: flex;
   flex-direction: column;
+
+  > span {
+    font-size: 18px;
+    margin-top: 30px;
+    color: white;
+
+    &:hover {
+      color: red;
+    }
+  }
 `;
 
 const Input = styled.input`
-  /* border: solid; */
-  padding: 0 16px;
-  border-radius: 16px 0;
+  border: 2px groove;
+  border-radius: 0 16px;
+  border-width: 5px;
+  padding: 3px 14px;
+  margin-bottom: 10px;
+  font-size: 23px;
+`;
+
+const FormLogin = styled.div`
+  background-color: var(--primary);
+  border-radius: 16px;
+  padding: 45px 90px;
+  opacity: 0.75;
+`;
+
+const StyledLink = styled(Link)`
+  color: #04f1bd;
+  padding-left: 5px;
+  font-size: 18px;
+
+  &:hover {
+    color: #f62fdd;
+  }
 `;
 
 function Register() {
@@ -79,40 +127,44 @@ function Register() {
 
   return (
     <SignUpContainer>
-      <form action='' onSubmit={registerButton} className='xx'>
-        <TitleContainer>建立新帳號</TitleContainer>
-        <InputDataContainer>使用者名稱</InputDataContainer>
-        <Input
-          type='text'
-          placeholder='Enter username'
-          onChange={changeHandler('name')}
-          value={registerInfo.name}
-          autoFocus
-          required
-        />
+      <FormLogin>
+        <form action='' onSubmit={registerButton} className='xx'>
+          <TitleContainer>建立新帳號</TitleContainer>
+          <InputDataContainer>使用者名稱</InputDataContainer>
+          <Input
+            type='text'
+            placeholder='Enter username'
+            onChange={changeHandler('name')}
+            value={registerInfo.name}
+            autoFocus
+            required
+          />
 
-        <InputDataContainer>電子郵件</InputDataContainer>
-        <Input
-          type='email'
-          placeholder='Enter email'
-          onChange={changeHandler('email')}
-          value={registerInfo.email}
-          required
-        />
+          <InputDataContainer>電子郵件</InputDataContainer>
+          <Input
+            type='email'
+            placeholder='Enter email'
+            onChange={changeHandler('email')}
+            value={registerInfo.email}
+            required
+          />
 
-        <InputDataContainer>密碼</InputDataContainer>
-        <Input
-          type='password'
-          placeholder='Enter password'
-          onChange={changeHandler('password')}
-          value={registerInfo.password}
-          required
-        />
-        <ButtonAndLink>
-          <RegisterButton type='submit'>註冊</RegisterButton>
-          <Link to='/login'>已經有一個帳號？</Link>
-        </ButtonAndLink>
-      </form>
+          <InputDataContainer>密碼</InputDataContainer>
+          <Input
+            type='password'
+            placeholder='Enter password'
+            onChange={changeHandler('password')}
+            value={registerInfo.password}
+            required
+          />
+          <ButtonAndLink>
+            <RegisterButton type='submit'>註冊</RegisterButton>
+            <span>
+              <StyledLink to='/login'>已經有一個帳號？</StyledLink>
+            </span>
+          </ButtonAndLink>
+        </form>
+      </FormLogin>
     </SignUpContainer>
   );
 }
