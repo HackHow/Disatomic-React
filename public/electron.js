91ce857804408e0014ce5ab072b63d1f2e7f6be8
Howard <howardshen2306@gmail.com>
2022-10-24 16:06:54 +0800

    feat: update electron settings

--- public/electron.js
+++ public/electron.js
@@ -27,7 +27,7 @@ function createWindow() {
   // win.loadFile("index.html");
   win.loadURL(
     isDev
-      ? 'http://localhost:3000/'
+      ? 'http://localhost:3000'
       : `file://${path.join(__dirname, '../build/index.html')}`
   );
   // Open the DevTools.
